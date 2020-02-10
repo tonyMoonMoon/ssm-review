d42089755928ab86b6a6af90bf3b69a6b23462e6
tonyMoonMoon <1733133070@qq.com>
2020-02-10 15:23:57 +0800

    第一次提交

---
+++ src/main/java/net/wanho/controller/HelloController.java
@@ -0,0 +1,17 @@
+package net.wanho.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String hello(Map map){
+        map.put("msg","hello");
+        return "hello";
+    }
+}
